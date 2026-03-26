2ea7a2c5c42b2e4d43250b3d639bc041ddc38781
Sandro Peixoto <contato@sandropeixoto.com.br>
2026-03-26 13:11:25 -0300

    refactor: remove redundant Representation section and update navigation

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Partners from './components/Partners';
 import About from './components/About';
 import Materials from './components/Materials';
 import Social from './components/Social';
-import Representation from './components/Representation';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import SEO from './components/SEO';
@@ -17,7 +16,6 @@ function App() {
       <Navbar />
       <main>
         <Hero />
-        <Representation />
         <Social />
         <Services />
         <Partners />
